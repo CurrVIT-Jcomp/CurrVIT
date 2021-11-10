cb19bdb7591c1465888dd4f69ddbacc2a02cb6d8
SRITAM MISHRA <79492275+ENVIRYO2112VIT@users.noreply.github.com>
2021-11-10 14:33:11 +0530

    Added Currency Details in ERC Contract

--- token_sale/contracts/CurrVIT.sol
+++ token_sale/contracts/CurrVIT.sol
@@ -3,20 +3,25 @@ pragma solidity >=0.4.22 <0.9.0;
 
 //ALL FN AND VAR NAMES ARE AS PER ERC 20 STD.  
 contract CurrVIT{
+    //Name
+    string public name='CurrVIT';
+    //Symbol
+    string public symbol='CVT';
     //Constuctor: Initializer
-    
+    string public ver='CurrVIT v1';
     //Read total no. of tokens
 
     //totalSupply is a state variable accessible to all
     //Set tokens
     uint256 public totalSupply; //Declared VAR NAME NOT TO BE CHOSEN ARBITRARILY 
 
-    constructor() public{  
+    //local variables in Solidity start with '_'
+    constructor(uint256 _inittotalSupply) public{  
          //made public so as to run as when the contract tis deployed
-         totalSupply= 1000000;
-         
+         balanceOf[msg.sender]=_inittotalSupply;
+         totalSupply= _inittotalSupply;
+    }
 
-     }
-
-
+    mapping(address=>uint256) public balanceOf;
 }
+
