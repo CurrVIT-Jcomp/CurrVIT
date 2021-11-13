81511cba8dfa76181a9d5953b11727ab45a3a71e
SRITAM MISHRA <79492275+ENVIRYO2112VIT@users.noreply.github.com>
2021-11-13 09:02:05 +0530

    Added Transfer function and event

--- token_sale/contracts/CurrVIT.sol
+++ token_sale/contracts/CurrVIT.sol
@@ -17,11 +17,45 @@ contract CurrVIT{
 
     //local variables in Solidity start with '_'
     constructor(uint256 _inittotalSupply) public{  
-         //made public so as to run as when the contract tis deployed
+         //made public so as to run as when the contract is deployed
          balanceOf[msg.sender]=_inittotalSupply;
          totalSupply= _inittotalSupply;
     }
 
+    //Accounts and its balance stored as key:value pairs
     mapping(address=>uint256) public balanceOf;
+
+    //Transfer fn
+    //return bool
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+    
+    function transfer(address _to,uint256 _value) public returns(bool success){
+        //If sender account bal>value to be sent,
+        //continue with execution oflines below
+        //if false,then stop execution and return any gas used 
+        require(balanceOf[msg.sender]>=_value);
+        //transfer the balance
+        balanceOf[msg.sender]-=_value;
+        balanceOf[_to]+=_value;
+
+        //Transfer event
+        emit Transfer(msg.sender,_to,_value);
+        return true;
+    }
+
+    /*delegated transfer: Transfer from one acc to another without 
+    requirement of account owner
+    fns:
+      1 approve transfer
+      2 transfer
+    */
+
+
+    
 }
 
